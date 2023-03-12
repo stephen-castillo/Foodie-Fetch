ed7e2691aac31766648d65c403039d33383e0486
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-03-12 01:10:14 -0600

    Re-worked API call and parameter creation

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,73 @@
-console.log('Javascript working');
+//console.log('Javascript working');
+
+//javascrpt get form
+const form = document.querySelector('form');
+
+//function to get form data and reset on submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevent the default form submission
+
+    //create new form object on event.target  
+    var formData = new FormData(form); 
+
+    //add data to form object from input entries
+    var data = {};
+    formData.forEach((value, key) => {
+        if (value && key !== 'health[]') { // Only add fields that have a value
+          data[key] = value;
+        }
+    });
+    //console.log('New Data: ');console.log(data);
+
+    //get all checkboxes that are checked
+    const checkboxes = form.querySelectorAll('input[type="checkbox"][name="health[]"]:checked');
+    let healthParams = '';
+    checkboxes.forEach((checkbox, index) => {
+        if (index > 0) {
+            healthParams += '&'; //add & symbol
+        }
+        healthParams += `health=${encodeURIComponent(checkbox.value)}`; //Add checkbox value to sting
+    });
+    //console.log(healthParams);
+    
+    //add app_id, app_key, search type
+    data.type='public';
+    data.app_key = 'f20b386e34889b19ea6df95568e0ae4f';
+    data.app_id = '172c19d1';
+
+    //reset the form data after submit
+    form.reset();
+
+    //log out the form data
+    //console.log('Form data:', data);
+
+    //convert form data object into url parameters minus checkbox values
+    var inputs = new URLSearchParams(data);
+    //make inputs into string
+    inputs = inputs.toString();
+    
+    //set substring for search and removal if search is empty
+    const subString = 'search=';
+
+    //get info from search bar
+    searchVal = $('#searchItem').val();
+    if(searchVal !== null || searchVal !== ''){
+        data.search=searchVal;
+        //console.log('added search');
+    }else{
+        inputs = inputs.replace(subString, '');
+        //console.log('removed search');
+    }
+
+
+    var params = inputs+'&'+healthParams
+    console.log(params);
+    getRecipe(handleData, params);
+
+});
 
 //Recipe API sample call
-var recSearch = 'Tuna fish sandwich';
-function getRecipe(callback, SearchParm){
+function getRecipe(callback, options){
 
     const url = 'https://api.edamam.com/api/recipes/v2';
     const params = {
@@ -12,14 +77,9 @@ function getRecipe(callback, SearchParm){
         }
     };
 
-    const query = new URLSearchParams({
-        type: 'public',
-        app_key: 'f20b386e34889b19ea6df95568e0ae4f',
-        app_id: '172c19d1',
-        q: SearchParm,
-    });
-
-    fetch(`${url}?${query.toString()}`, params)
+    //console.log('what sending: '+`${url}?${query.toString()}`, params);
+    console.log('what sending: '+`${url}?${options}`, params);
+    fetch(`${url}?${options}`, params)
     .then(response => response.json())
     .then(data => {
         //console.log(data);
@@ -30,33 +90,10 @@ function getRecipe(callback, SearchParm){
     .catch(error => console.error(error));
 }
 
+//function to handle returned data
 function handleData(data){
     console.log(data);
 }
-
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // prevent the default form submission
-
-    //create new form object on event.target  
-    const formData = new FormData(event.target); // get the form data
-    
-    //add data to form object from input entries
-    const data = Object.fromEntries(formData.entries()); // convert the FormData object to a plain object
-    const data2 = formData.getAll('health');
-
-    //console.log('Form data:', data);
-    //console.log('checkboxes:', data2);
-
-    //add checkboxes into form data object
-    data.health = data2;
-
-    //get infor from search bar
-    data.search = $('#searchItem').val();
-    
-    console.log('Form data:', data);
-});
 
 // Hide recipe section for now
 document.getElementById('recipe-view').style.display='none';
