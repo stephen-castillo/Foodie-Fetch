2f339e953140e43b287acc19ff74ab06ebe7c549
unknown <sandra.gutierrez.ru@gmail.com>
2023-03-14 19:42:03 -0500

    non working local storage buttons

--- assets/script.js
+++ assets/script.js
@@ -111,7 +111,7 @@ function handleData(data){
         $('#recipe-list').append('<div class="recipe_modal" id="recipe'+[i]+'"></div>');
         //create new div with '<div id="recipe'+[i]+'">
         //console.log(cRes.images.REGULAR.url);
-        $('#recipe'+[i]).append('<h4 class="modal_header"><b>'+cRes.label+'</b></h4><br><figure class="modal_image"><img src="'+cRes.images.REGULAR.url+'" alt="Picture of '+cRes.label+'" height="300px" width="300px"></figure><br><br>');
+        $('#recipe'+[i]).append('<h4 class="modal_header"><b>'+cRes.label+'</b></h4><br><button class="button" id="saveBtn"></button><figure class="modal_image"><img src="'+cRes.images.REGULAR.url+'" alt="Picture of '+cRes.label+'" height="300px" width="300px"></figure><br><br>');
         
         var $newList = $('<ul>').attr('id', 'recList'+[i]);
         $newList.append($('<b><caption class="modal_ingred">').text('Ingredients'));
@@ -121,14 +121,31 @@ function handleData(data){
         }
         $('#recipe'+[i]).append('<br><a class="modal_link" href="'+cRes.url+'" target="_blank">How to prepare '+cRes.label+'</a><hr style="height:5px; background-color: #787e87;">');
     
-    }
+    $('#saveBtn').click(function(){
+       cRes = JSON.stringify(cRes);
+       localStorage.setItem(cRes.label, cRes);
+    })
     console.log($('#recipe-list').html());
 }
 //getRecipe('chicken salad');
 
 console.log('Data here');
 //getRecipe(handleData, recSearch);
+}
 
+//function showArchive(){
+//    var archive = Object.keys(localStorage).sort();
+//    for(i=0; i<archive.length; i++){
+ //       var recipeKey = archive[i];
+ //       var havethis = localStorage.getItem(archive[i]);
+ //       value = JSON.parse(cRes);
+ //       //format the value data
+
+        //make the formated value data a variable;
+   //     var formatted = 'formatted data'; //actually put the data here
+  //      $('#archive').append(formatted);
+   // }
+//}
 
 // mobile-responsive menu //
 const burgerIcon =document.querySelector('#burger');
